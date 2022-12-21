3533f3c35884581eb624687f9c46f86d13f1662b
hydra <zmbakh@gmail.com>
2022-12-21 21:30:27 +0600

    Sorting the order history table by the cart name

---
+++ src/Pyz/Zed/Sales/Persistence/Propel/QueryBuilder/OrderSearchFilterFieldQueryBuilder.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Sales\Persistence\Propel\QueryBuilder;
+
+use Orm\Zed\Sales\Persistence\Map\SpySalesOrderTableMap;
+use Spryker\Zed\Sales\Persistence\Propel\QueryBuilder\OrderSearchFilterFieldQueryBuilder as SprykerOrderSearchFilterFieldQueryBuilder;
+
+class OrderSearchFilterFieldQueryBuilder extends SprykerOrderSearchFilterFieldQueryBuilder
+{
+    /**
+     * @var array<string, string>
+     */
+    protected const ORDER_BY_COLUMN_MAPPING = [
+        self::SEARCH_TYPE_ORDER_REFERENCE => SpySalesOrderTableMap::COL_ID_SALES_ORDER,
+        'date' => SpySalesOrderTableMap::COL_CREATED_AT,
+        'pyzCartName' => SpySalesOrderTableMap::COL_PYZ_CART_NAME,
+    ];
+}
